d79a3cac8ad626d7fca776ebafd5febc2334bdaa
Lina Garcia <garcia.j.lina@gmail.com>
2016-08-04 18:59:15 +0100

    added nested form in cocktail new, takes doses directly when created

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -2,4 +2,5 @@ class Cocktail < ActiveRecord::Base
   has_many :doses, dependent: :destroy
   has_many :ingredients, through: :doses
   validates :name, presence: true, uniqueness: true
+  accepts_nested_attributes_for :doses
 end
