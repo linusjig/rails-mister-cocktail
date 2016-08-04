e50f188e5806f86793f65bdddb363a3deb446084
Lina Garcia <garcia.j.lina@gmail.com>
2016-08-04 11:44:04 +0100

    add model dose with references to cocktail and ingredient, changed model ingridient to ingredient

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,5 +1,5 @@
 class Cocktail < ActiveRecord::Base
   has_many :doses, dependent: :destroy
-  has_many :ingridients, through: :doses
+  has_many :ingredients, through: :doses
   validates :name, presence: true, uniqueness: true
 end
--- app/models/ingridient.rb
+++
@@ -1,2 +0,0 @@
-class Ingridient < ActiveRecord::Base
-end
